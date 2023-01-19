4b04bb6e1d1c872203d077898c10147c03d9d247
Thanathorn M <thanathorn1073@gmail.com>
2023-01-19 13:53:25 +0700

    init project.
    flutter create new project which following https://github.com/ShadyBoukhary/flutter_clean_architecture & https://betterprogramming.pub/flutter-clean-architecture-test-driven-development-practical-guide-445f388e8604 architecture.

---
+++ android/app/src/main/kotlin/com/example/trek_bkk_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trek_bkk_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("trek-bkk"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
