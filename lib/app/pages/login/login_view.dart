b86e84a35234fd911ddc36c4dbd1a5f4734a142e
Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
2023-02-26 14:36:20 +0700

    Update login_view.dart

--- lib/app/pages/login/login_view.dart
+++ lib/app/pages/login/login_view.dart
@@ -24,35 +24,42 @@ class SignInPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Image(
-            image: AssetImage("assets/icons/signin.png"),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(
+                image: AssetImage("assets/icons/signin.png"),
+              ),
+              const SizedBox(height: 48),
+              const Text(
+                  textAlign: TextAlign.center,
+                  "Sign in to see your favorite list \nand propose your own route!"),
+              const SizedBox(height: 48),
+              OutlinedButton.icon(
+                onPressed: google_signIn,
+                icon: const Image(
+                    width: 16, image: AssetImage("assets/icons/google.png")),
+                label: const Text("Sign in with Google"),
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.resolveWith((states) =>
+                        const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 16))),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Image(
+                    width: 16, image: AssetImage("assets/icons/github.png")),
+                label: const Text("Sign in with Github"),
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.resolveWith((states) =>
+                        const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 16))),
+              )
+            ],
           ),
-          const SizedBox(height: 48),
-          const Text(
-              textAlign: TextAlign.center,
-              "Sign in to see your favorite list \nand propose your own route!"),
-          const SizedBox(height: 48),
-          OutlinedButton.icon(
-            onPressed: google_signIn,
-            icon: const Image(
-                width: 16, image: AssetImage("assets/icons/google.png")),
-            label: const Text("Sign in with Google"),
-            style: ButtonStyle(
-                padding: MaterialStateProperty.resolveWith((states) =>
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 16))),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            icon: const Image(
-                width: 16, image: AssetImage("assets/icons/github.png")),
-            label: const Text("Sign in with Github"),
-            style: ButtonStyle(
-                padding: MaterialStateProperty.resolveWith((states) =>
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 16))),
-          )
         ],
       )),
     );
