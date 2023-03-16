c0f22b0a2f361c12bfabd804de467500c5a8ce07
WalanGun <ryw36237@gmail.com>
2023-03-16 17:31:15 +0700

    [feat] filter search result

--- lib/app/pages/search/search_2.dart
+++ lib/app/pages/search/search_2.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:trek_bkk_app/app/utils/limit_range_text_input_formatter.dart';
+import 'package:trek_bkk_app/app/utils/search_result_dummy.dart';
+import 'package:trek_bkk_app/app/pages/search/filter_search_results.dart';
+
+import '../../../constants.dart';
+
+List<String> tags = [
+  "home",
+  "alone",
+  "sad",
+  "veryverysad",
+  "okokokkkkkkkk",
+  "new line"
+];
 
 class Search2 extends StatefulWidget {
   final String initialSearchKey;
@@ -10,9 +25,31 @@ class Search2 extends StatefulWidget {
 }
 
 class _Search2State extends State<Search2> {
+  int _numStopsSliderValue = 10;
+  List<String> queryTagList = tags.toList();
+  List<String> selectedTagList = tags.toList();
+  Color tagColor = lightColor;
+
+  late final TextEditingController _numStopsTextFieldController;
+  final searchResultList = searchResults;
+
+  @override
+  void initState() {
+    super.initState();
+    _numStopsTextFieldController = TextEditingController();
+    _numStopsTextFieldController.text = "10";
+  }
+
+  @override
+  void dispose() {
+    _numStopsTextFieldController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     String searchKey = widget.initialSearchKey;
+    var filteredSearchResult = searchResultList;
 
     return Scaffold(
       body: Column(
@@ -54,26 +91,7 @@ class _Search2State extends State<Search2> {
                       width: 24,
                     ),
                     GestureDetector(
-                      onTap: () => showDialog(
-                          context: context,
-                          builder: (BuildContext context) => Dialog(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(24),
-                                  child: Column(
-                                    children: [
-                                      const Align(
-                                          alignment: Alignment.topRight,
-                                          child: CloseButton()),
-                                      ElevatedButton(
-                                        child: const Text("APPLY FILTER"),
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              )),
+                      onTap: () => _filterDialogBuilder(context),
                       child: const Icon(Icons.filter_alt),
                     )
                   ],
@@ -81,12 +99,152 @@ class _Search2State extends State<Search2> {
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(24),
-            child: Container(),
+          Expanded(
+            child: ListView.builder(
+                itemCount: filteredSearchResult.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: Container(
+                      height: 100,
+                      color: const Color.fromARGB(215, 2, 173, 252),
+                      child: Column(
+                        children: [
+                          Text('name: ${filteredSearchResult[index]['name']}'),
+                          Text(
+                              'distance: ${filteredSearchResult[index]['distance']}'),
+                          Text('stop: ${filteredSearchResult[index]['stop']}'),
+                          Text(
+                              'description: ${filteredSearchResult[index]['description']}'),
+                          Text(
+                              'tag_set: ${filteredSearchResult[index]['tag_set'].join(', ')}'),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
           ),
         ],
       ),
+    );
+  }
+
+  Future<void> _filterDialogBuilder(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return StatefulBuilder(
+            builder: (BuildContext stfContext, StateSetter stfSetState) {
+          return Dialog(
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                children: [
+                  const Align(
+                      alignment: Alignment.topRight, child: CloseButton()),
+                  const Text(
+                    "Maximum total distance",
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 96,
+                        child: TextFormField(
+                          textAlign: TextAlign.center,
+                          controller: _numStopsTextFieldController,
+                          decoration: InputDecoration(
+                              suffixText: "km",
+                              contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 8),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8))),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                            LimitRangeTextInputFormatter(1, 10)
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _numStopsSliderValue = int.parse(value);
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: Slider(
+                          max: 10,
+                          divisions: 10,
+                          value: _numStopsSliderValue.toDouble(),
+                          label: _numStopsSliderValue.round().toString(),
+                          onChanged: ((value) {
+                            stfSetState(() {
+                              _numStopsSliderValue = value.toInt();
+                              _numStopsTextFieldController.text =
+                                  _numStopsSliderValue.toString();
+                            });
+                          }),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    "Types of places",
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: queryTagList
+                        .map<Widget>((tag) => FilterChip(
+                              label: Text(tag),
+                              showCheckmark: false,
+                              selected: selectedTagList.contains(tag),
+                              onSelected: (bool selected) {
+                                stfSetState(() {
+                                  if (selected) {
+                                    if (!selectedTagList.contains(tag)) {
+                                      selectedTagList.add(tag);
+                                    }
+                                  } else {
+                                    selectedTagList.remove(tag);
+                                  }
+                                });
+                              },
+                              selectedColor: lightColor,
+                              disabledColor: Colors.grey.shade100,
+                            ))
+                        .toList(),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  ElevatedButton(
+                    child: const Text("APPLY FILTER"),
+                    onPressed: () {
+                      var temp = filterSearchResult(_numStopsSliderValue,
+                          selectedTagList, searchResultList);
+                      for (var t in temp) {
+                        print(t['name']);
+                      }
+                      // print('${temp.length}');
+                      Navigator.pop(dialogContext);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+      },
     );
   }
 }
---
+++ lib/app/utils/search_result_dummy.dart
@@ -0,0 +1,72 @@
+List<Map<String, dynamic>> searchResults = [
+  {
+    'name': 'Route 1',
+    'distance': 10,
+    'stop': 10,
+    'description': 'Route 1\'s description!',
+    'tag_set': ["home", "alone"]
+  },
+  {
+    'name': 'Route 2',
+    'distance': 8,
+    'stop': 8,
+    'description': 'Route 2\'s description!',
+    'tag_set': ["sad", "veryverysad"]
+  },
+  {
+    'name': 'Route 3',
+    'distance': 6,
+    'stop': 6,
+    'description': 'Route 3\'s description!',
+    'tag_set': ["okokokkkkkkkk", "new line"]
+  },
+  {
+    'name': 'Route 4',
+    'distance': 4,
+    'stop': 4,
+    'description': 'Route 4\'s description!',
+    'tag_set': ["home", "sad"]
+  },
+  {
+    'name': 'Route 5',
+    'distance': 2,
+    'stop': 2,
+    'description': 'Route 5\'s description!',
+    'tag_set': ["new line", "alone"]
+  },
+  {
+    'name': 'Route 6',
+    'distance': 9,
+    'stop': 9,
+    'description': 'Route 6\'s description!',
+    'tag_set': ["home", "veryverysad"]
+  },
+  {
+    'name': 'Route 7',
+    'distance': 7,
+    'stop': 7,
+    'description': 'Route 7\'s description!',
+    'tag_set': ["okokokkkkkkkk", "veryverysad"]
+  },
+  {
+    'name': 'Route 8',
+    'distance': 5,
+    'stop': 5,
+    'description': 'Route 8\'s description!',
+    'tag_set': ["okokokkkkkkkk", "home"]
+  },
+  {
+    'name': 'Route 9',
+    'distance': 3,
+    'stop': 3,
+    'description': 'Route 9\'s description!',
+    'tag_set': ["home", "new line"]
+  },
+  {
+    'name': 'Route 10',
+    'distance': 1,
+    'stop': 1,
+    'description': 'Route 10\'s description!',
+    'tag_set': ["veryverysad", "alone"]
+  },
+];
