61b6ee9fa8e109a02892d0d4257bd406bdd84ac4
Thanathorn M <thanathorn1073@gmail.com>
2023-05-14 13:33:00 +0700

    feat(auth): add another way for login.

--- lib/app/pages/login/login_view.dart
+++ lib/app/pages/login/login_view.dart
@@ -4,9 +4,18 @@ import 'package:trek_bkk_app/domain/repositories/auth_services.dart';
 import 'package:trek_bkk_app/providers/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  bool _showLoginAsGuest = false;
+  final _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +26,50 @@ class SignInPage extends StatelessWidget {
         await Provider.of<UserData>(context, listen: false)
             .getUser(res.displayName!, res.email, res.photoURL ?? "");
       }
+    }
+
+    Future signInGuest(String name) async {
+      if (context.mounted) {
+        DateTime now = DateTime.now();
+        String formattedDate = DateFormat('ddMMyyyyHHmmss').format(now);
+        String email = '$name$formattedDate@tempmail.com';
+        await Provider.of<UserData>(context, listen: false)
+            .getUser(name, email, "");
+      }
+    }
+
+    void _showPopup() {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Enter Your Name"),
+            content: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: "Your Name",
+                      border: OutlineInputBorder(),
+                      labelText: "Name",
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  child: Text("Send"),
+                  onPressed: () {
+                    String name = _textController.text;
+                    signInGuest(name);
+                  },
+                ),
+              ],
+            ),
+          );
+          ;
+        },
+      );
     }
 
     return Scaffold(
@@ -50,15 +103,48 @@ class SignInPage extends StatelessWidget {
                 height: 8,
               ),
               OutlinedButton.icon(
-                onPressed: () {},
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text("Enter Your Name"),
+                        content: Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: _textController,
+                                decoration: InputDecoration(
+                                  hintText: "Your Name",
+                                  border: OutlineInputBorder(),
+                                  labelText: "Name",
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            ElevatedButton(
+                              child: Text("Send"),
+                              onPressed: () async {
+                                String name = _textController.text;
+                                signInGuest(name);
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                      ;
+                    },
+                  );
+                },
                 icon: const Image(
                     width: 16, image: AssetImage("assets/icons/github.png")),
-                label: const Text("Sign in with Github"),
+                label: const Text("Sign in with e-mail"),
                 style: ButtonStyle(
                     padding: MaterialStateProperty.resolveWith((states) =>
                         const EdgeInsets.symmetric(
                             horizontal: 40, vertical: 16))),
-              )
+              ),
             ],
           ),
         ],
